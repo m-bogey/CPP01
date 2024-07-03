65a2c25f19cb2432baec80f322cad9f46d556439
root <root@mbogey.mbogey.virtualbox.org>
2024-07-03 21:03:15 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "Memory adress of the string variable: " << &str << std::endl;
+    std::cout << "Memory adress help by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory adress help by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Value of the string variable: " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
